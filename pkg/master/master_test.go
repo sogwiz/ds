dc232f4987ec78fee05cbc8521b664f0360739a0
Alain Gilbert <alain.gilbert.15@gmail.com>
2019-09-08 22:47:44 -0700

    Add assert library

--- pkg/master/master_test.go
+++ pkg/master/master_test.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"io/ioutil"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPutFile(t *testing.T) {
 	fileContent, _ := ioutil.ReadFile("/Users/agilbert/go/src/ds/main.go")
 	PutFile(UserID(1), "myfile.txt", bytes.NewReader(fileContent))
-	t.Fail()
+	assert.True(t, false)
 }
