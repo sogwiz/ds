2edda82a895f53573e5e3d0c45c71552e2013aab
Alain Gilbert <alain.gilbert.15@gmail.com>
2019-09-09 20:39:33 -0700

    handle file not found error in client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,11 +5,17 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	homeDir, _ := os.UserHomeDir()
-	fileContent, _ := ioutil.ReadFile(filepath.Join(homeDir, "go", "src", "ds", "cmd", "ds", "main.go"))
+	path := filepath.Join(homeDir, "go", "src", "ds", "cmd", "ds", "main.go")
+	fileContent, err := ioutil.ReadFile(path)
+	if err != nil {
+		logrus.Fatal("file not found", err)
+	}
 
 	conn, err := net.Dial("tcp", "127.0.0.1:3300")
 	if err != nil {
