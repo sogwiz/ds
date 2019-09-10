d54f3f7268c5ec67c584538148574c59efb7f522
Alain Gilbert <alain.gilbert.15@gmail.com>
2019-09-09 20:51:34 -0700

    greatly improve performance of client, when streaming big files

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io/ioutil"
+	"io"
 	"net"
 	"os"
 	"path/filepath"
@@ -10,18 +10,21 @@ import (
 )
 
 func main() {
+	masterHostname := "127.0.0.1:3300"
+
 	homeDir, _ := os.UserHomeDir()
 	path := filepath.Join(homeDir, "go", "src", "ds", "cmd", "ds", "main.go")
-	fileContent, err := ioutil.ReadFile(path)
+	f, err := os.Open(path)
 	if err != nil {
 		logrus.Fatal("file not found", err)
 	}
+	defer f.Close()
 
-	conn, err := net.Dial("tcp", "127.0.0.1:3300")
+	conn, err := net.Dial("tcp", masterHostname)
 	if err != nil {
 		panic(err)
 	}
 
 	_, _ = conn.Write([]byte("user_1/myfile.txt|"))
-	_, _ = conn.Write(fileContent)
+	_, _ = io.Copy(conn, f)
 }
