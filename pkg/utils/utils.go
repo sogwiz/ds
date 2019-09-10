08844cd5aace8d3b996d786e9052f871d6995a69
Alain Gilbert <alain.gilbert.15@gmail.com>
2019-09-10 00:36:21 -0700

    Add doc

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,6 +6,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// AcceptConn hack to return the conn from a channel.
+// We can now wait for a conn in a select statement.
 func AcceptConn(l net.Listener) <-chan net.Conn {
 	ch := make(chan net.Conn)
 	go func() {
