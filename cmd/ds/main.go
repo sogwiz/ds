7de99521ce31009409db6348052da739777b6190
Alain Gilbert <alain.gilbert.15@gmail.com>
2019-09-09 21:11:46 -0700

    missing ds go file

---
+++ cmd/ds/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"ds/pkg/actions"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"gopkg.in/urfave/cli.v2"
+)
+
+func main() {
+	app := &cli.App{
+		Action: func(c *cli.Context) error {
+			logrus.Info("run master or slave")
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name:   "master",
+				Usage:  "Starts the master node",
+				Action: actions.StartMaster,
+			},
+			{
+				Name:   "slave",
+				Usage:  "Starts the slave node",
+				Action: actions.StartSlave,
+			},
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		logrus.Fatal(err)
+	}
+}
